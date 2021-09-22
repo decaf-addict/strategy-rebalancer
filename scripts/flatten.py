7b7580ff17d210be0a6b435009a36097ef504840
Tonkers Kuma <tonkers.society@gmail.com>
2021-09-21 18:03:58 -0700

    flatten script

--- scripts/flatten.py
+++ scripts/flatten.py
@@ -1,5 +1,8 @@
-from brownie import Rebalancer, accounts, config, network, project, web3
+from brownie import Rebalancer, JointProvider, accounts, config, network, project, web3
+
 
 def main():
-    with open('./RebalancerFlat.sol', 'w') as f:
+    with open('./build/contracts/RebalancerFlat.sol', 'w') as f:
         f.write(Rebalancer.get_verification_info()['flattened_source'])
+    with open('./build/contracts/JointProviderFlat.sol', 'w') as f:
+        f.write(JointProvider.get_verification_info()['flattened_source'])
